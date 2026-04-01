54ee0df7fda12bb98baa02ac31122a4427839d63
DigitalRage <blaine.buckler@ucas-edu.net>
2026-04-01 09:51:43 -0600

    YES TKINTER!!!

---
+++ testing/customtkintertesting.py
@@ -0,0 +1,29 @@
+import customtkinter
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        customtkinter.set_appearance_mode("dark")
+        self.title("Fullscreen App")
+        self.attributes('-fullscreen', True)
+        self.bind("<Escape>", lambda event: self.attributes("-fullscreen", False))
+
+        label = customtkinter.CTkLabel(master=self, text="Welcome to Fullscreen App! Press Esc to exit.")
+        label.pack(fill="both", expand=True)
+    
+    def button(self, name):
+        self.btn = customtkinter.CTkButton(
+            master=self,
+            text=name,
+            command=lambda: print("Button clicked"),
+            width=10000,
+            height=50,
+            border_width=200,
+            corner_radius=2,
+            hover=True
+        )
+        self.btn.pack(pady=20)
+        
+app = App()
+app.button(name="Test")
+app.mainloop()
