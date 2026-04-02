59076102e0b98fde34b788d2902e370a46964093
Yourinthemood <ulziibayardarin@gmail.com>
2026-04-01 21:37:23 -0600

    Add pseudocode for graphing balances
    Added pseudocode for graphing balances from a CSV file.

---
+++ docs/psuedocode/graphscreen.py
@@ -0,0 +1,13 @@
+# IMPORT pandas and matplotlib
+
+# READ the CSV file
+
+# GET date column and balance column
+
+# GET the last 10 entries
+
+# PLOT x = dates, y = balances
+
+# ADD labels and title
+
+# DISPLAY graph on screen
